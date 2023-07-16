152628d124de99465bd19f3be769d56162da3305
Yoga Adi Saputra <syogaadi75@gmail.com>
2023-07-16 18:21:28 +0700

    fix errors

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,14 @@ import App from './App'
 import { Provider } from 'react-redux'
 import { store } from './redux/store'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <Router>
         <App />
-      </BrowserRouter>
+      </Router>
     </Provider>
   </React.StrictMode>
 )
